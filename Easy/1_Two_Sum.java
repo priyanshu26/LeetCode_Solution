b568f0dd98fc27e68a69e24982e02d5c8e79748d
Priyanshu Shukla <shuklaps1119@gmail.com>
2021-04-09 10:29:23 +0530

    Create 1_Two_Sum.java

---
+++ Easy/1_Two_Sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap();
+        
+        // fill HM
+        for(int i = 0; i<nums.length; i++){
+            map.put(nums[i], i);
+        }
+        // Searching
+        for(int i = 0; i<nums.length; i++){
+            int num = nums[i];
+            int rem = target - num;
+            if(map.containsKey(rem)){
+                int index = map.get(rem);
+                if(index==i)continue;
+                return new int[]{i,index};
+            } 
+        }
+        return new int[]{};
+    }
+}
